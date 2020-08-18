f1cbc3356b05566d59559a73ffd85b20ef973c51
Bhargav Annem <bhargavannem@Bhargavs-iMac.local>
2020-08-18 15:19:42 -0500

    basic functionality without stopping

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -5,36 +5,22 @@ export default class Bar extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.runSimulation = this.runSimulation.bind(this);
+
     this.simulate = this.simulate.bind(this);
     this.getNumAliveNeighbors = this.getNumAliveNeighbors.bind(this);
   }
 
-  getNumAliveNeighbors(r, c) {
+  getNumAliveNeighbors(R, C) {
     var cnt = 0;
-    if (
-      c - 1 >= 0 &&
-      document.getElementById("" + r + "-" + (c - 1)).className === "alive"
-    ) {
-      cnt++;
-    }
-    if (
-      c + 1 < 100 &&
-      document.getElementById("" + r + "-" + (c + 1)).className === "alive"
-    ) {
-      cnt++;
-    }
-    if (
-      r - 1 >= 0 &&
-      document.getElementById("" + (r - 1) + "-" + c).className === "alive"
-    ) {
-      cnt++;
-    }
-    if (
-      r + 1 < 75 &&
-      document.getElementById("" + (r + 1) + "-" + c).className === "alive"
-    ) {
-      cnt++;
+    for (var i = -1; i <= 1; i++) {
+      for (var j = -1; j <= 1; j++) {
+        if (
+          document.getElementById("" + (R + i) + "-" + (C + j)).className ==
+          "alive"
+        ) {
+          cnt++;
+        }
+      }
     }
     return cnt;
   }
@@ -47,39 +33,78 @@ export default class Bar extends Component {
   //   - Any live cell with two or three live neighbours lives on to the next generation.
   //   - Any live cell with more than three live neighbours dies, as if by overpopulation.
   //   - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-  simulate() {
+
+  blankArray() {
+    var ele = [];
+    for (var i = 0; i < 75; i++) {
+      var tmp = [];
+      for (var j = 0; j < 100; j++) {
+        tmp.push(0);
+      }
+      ele.push(tmp);
+    }
+    return ele;
+  }
+  buildArray() {
+    var mat = this.blankArray();
+
     for (var r = 0; r < 75; r++) {
       for (var c = 0; c < 100; c++) {
-        var ele = document.getElementById("" + r + "-" + c);
-
-        var isAlive;
-        if (ele.className === "dead") {
-          isAlive = false;
-        } else {
-          isAlive = true;
+        if (document.getElementById("" + r + "-" + c).className == "alive") {
+          mat[r][c] = 1;
         }
-        var numAliveNeighbors = this.getNumAliveNeighbors(r, c);
+      }
+    }
+
+    // console.log(mat);
+    return mat;
+  }
+
+  simulate() {
+    var future = this.blankArray();
+    var grid = this.buildArray();
+
+    for (var l = 1; l < 74; l++) {
+      for (var m = 1; m < 99; m++) {
+        var aliveNeighbours = 0;
+        for (var i = -1; i <= 1; i++) {
+          for (var j = -1; j <= 1; j++) {
+            aliveNeighbours += grid[l + i][m + j];
+          }
+        }
+        aliveNeighbours -= grid[l][m];
+        // Cell is lonely and dies
+        if (grid[l][m] == 1 && aliveNeighbours < 2) future[l][m] = 0;
+        // Cell dies due to over population
+        else if (grid[l][m] == 1 && aliveNeighbours > 3) future[l][m] = 0;
+        // A new cell is born
+        else if (grid[l][m] == 0 && aliveNeighbours == 3) future[l][m] = 1;
+        // Remains the same
+        else future[l][m] = grid[l][m];
+      }
+    }
+    for (var i = 0; i < 75; i++) {
+      for (var j = 0; j < 100; j++) {
+        if (future[i][j] == 1) {
+          document.getElementById("" + i + "-" + j).className = "alive";
+        } else {
+          document.getElementById("" + i + "-" + j).className = "dead";
+        }
       }
     }
   }
-  runSimulation(toggle) {
-    var ele;
-    if (toggle === true) {
-      console.log("Turning on...");
-      ele = setInterval(this.simulate.bind(this), 2000);
-    } else {
-      console.log("Turning off...");
-      clearInterval(ele);
-    }
-  }
+
   handleClick() {
     var btn = document.getElementById("button");
-    if (btn.className === "active") {
-      btn.className = "unactive";
-      this.runSimulation(false);
-    } else {
+    var ele;
+    if (btn.className === "inactive") {
       btn.className = "active";
-      this.runSimulation(true);
+      console.log("Starting simulator...");
+      ele = setInterval(this.simulate.bind(this), 19);
+    } else {
+      console.log("Stopping simulator...");
+      btn.className = "inactive";
+      clearInterval(ele);
     }
   }
 
@@ -88,7 +113,7 @@ export default class Bar extends Component {
       <div id="bar">
         <button
           id="button"
-          className="unactive"
+          className="inactive"
           onClick={this.handleClick}
         ></button>
       </div>
--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import "./Box.css";
 
-
 export default class Box extends Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { className: "dead" };
   }
-  handleClick() {
-    console.log(this.props.id);
+  handleChange() {
+    if (document.getElementById("button").className === "inactive") {
+      if (this.state.className == "dead") {
+        this.setState({ className: "alive" });
+      } else {
+        this.setState({ className: "dead" });
+      }
+    }
   }
   render() {
-    return <td className="box" id={this.props.id} onClick={this.handleClick}></td>;
+    return (
+      <td
+        className={this.state.className}
+        id={this.props.id}
+        onClick={this.handleChange}
+        
+      ></td>
+    );
   }
 }
